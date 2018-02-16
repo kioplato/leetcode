d13887edb59dcc5ef7123eac548746d2645004f9
Plato Kiorpelidis <otalpster@gmail.com>
2018-02-17 00:34:06 +0200

    Change problem 46 to return the result instead of printing it.

--- 46_Permutations.py
+++ 46_Permutations.py
@@ -1,22 +1,24 @@
 # Python2.7
 # 46_Permutations.py
 
-def permutate(ordered_list, breakpoint):
-    print("%s") % (''.join(ordered_list))
+def permute(ordered_list, breakpoint = -1):
+    permutations_list = []
+    permutations_list.append(ordered_list)
     list_len = len(ordered_list)
     perm_head = list_len - 2
     perm_tail = list_len - 1
     while perm_head > breakpoint:
         perm_list = list(ordered_list)
         perm_list.insert(perm_head, perm_list.pop(perm_tail))
-        permutate(perm_list, perm_head)
+        permutations_list.extend(permute(perm_list, perm_head))
         if perm_tail == list_len - 1:
             perm_head = perm_head - 1
             perm_tail = perm_head + 1
         else:
             perm_tail = perm_tail + 1
+    return permutations_list
 
 if __name__ == '__main__':
     number_arr = raw_input("Enter a number to permutate: ")
     number_list = sorted(number_arr)
-    permutate(number_list, -1);
+    print(permute(number_list))
