611dd9f7c7abf3a57771a1e91653ed4a229e1770
Plato <otalpster@gmail.com>
2019-04-27 16:51:40 +0300

    problem70.py: solve problem 70
    Used solution is slow and gets time limit exceeded in leetcode.com.

---
+++ problem70.py
@@ -0,0 +1,20 @@
+# 70. Climbing stairs
+
+# Even though this solution yields correct results. it's slow and gets time
+# limit exceeded in leetcode.com.
+class Solution(object):
+    def climbStairs(self, n):
+        """
+        Returns the number of ways to climb n steps when you can climb one or
+        two each time.
+        :type n: int
+        :rtype: int
+        """
+        s = 0
+        if n == 0:
+            return 1
+        if n > 0:
+            s = s + self.climbStairs(n - 1)
+        if n > 1:
+            s = s + self.climbStairs(n - 2)
+        return s
